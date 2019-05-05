b994a70643f68c541fc4e59aa0a06bc4a901367b
Guenther <gzoppelt@gmail.com>
2019-05-05 07:17:11 +0100

    up to lecture 33

--- 01_basics/src/resolvers/Mutation.js
+++ 01_basics/src/resolvers/Mutation.js
@@ -1,4 +1,5 @@
 import uuidv4 from 'uuid/v4'
+import { WSASERVICE_NOT_FOUND } from 'constants';
 
 const Mutation = {
     createUser(parent, args, { db }, info) {
@@ -12,6 +13,32 @@ const Mutation = {
             ...args.data
         }
         db.users.push(user)
+
+        return user
+    },
+    updateUser(parent, args, { db }, info) {
+        const { id, data } = args
+        const user = db.users.find( (user) => user.id === id )
+
+        if (!user) {
+            throw new Error('User not found.')
+        }
+
+        if (typeof data.email === 'string') {
+            const emailTaken = db.users.some( (user) => user.email === data.email )
+            if (emailTaken) {
+                throw new Error('Email already in use.')
+            }
+            user.email = data.email
+        }
+
+        if (typeof data.name === 'string') {
+            user.name = data.name
+        }
+
+        if (typeof data.age !== 'undefined') { // null or Int
+            user.age = data.age
+        }
 
         return user
     },
@@ -52,6 +79,33 @@ const Mutation = {
 
         return post
     },
+    updatePost(parent, args, ctx, info) {
+        const { id, data } = args
+        const { db } = ctx
+        const post = db.posts.find( (post) => post.id === id )
+
+        if (!post) {
+            throw new Error('Post not found.')
+        }
+        
+        if (typeof data.title === 'string') {
+            post.title = data.title
+        }
+        
+        if (typeof data.title === 'string') {
+            post.title = data.title
+        }
+
+        if (typeof data.body === 'string') {
+            post.body = data.body
+        }
+
+        if (typeof data.published === 'boolean') {
+            post.published = data.published
+        }
+
+        return post
+    },
     deletePost(parent, args, { db }, info) {
         const postIndex = db.posts.findIndex( (post) => post.id === args.id )
         if (postIndex === -1) {
@@ -85,6 +139,21 @@ const Mutation = {
 
         return comment
     },
+    updateComment(parent, args, ctx, info) {
+        const {id, data} = args
+        const {db} = ctx
+        const comment = db.comments.find((comment) => comment.id = id)
+
+        if (!comment) {
+            throw new Error('Comment not found.')
+        }
+
+        if (typeof data.text === 'string') {
+            comment.text = data.text
+        }
+
+        return comment
+     },
     deleteComment(parent, args, { db }, info) {
         const commentIndex = db.comments.findIndex( (comment) => comment.id === args.id )
         if (commentIndex === -1) {
